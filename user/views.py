b694ff4ba6517742d1bfd7f84c8fee4fd90a377f
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2024-10-09 18:35:43 +0900

    ユーザ新規登録作成

--- user/views.py
+++ user/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from user.models import CustomUser
 
-# Create your views here.
+def signup(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    new_user = CustomUser(usrename=username, password=password)
+    new_user.save()
+    return HttpResponse('ユーザの作成に成功しました')
